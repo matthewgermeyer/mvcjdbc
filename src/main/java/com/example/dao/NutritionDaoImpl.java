6ac950e79e991f0a80166a960060ede0bf5896d2
Matt Germeyer <matto483@gmail.com>
2017-05-09 13:21:05 -0500

    mapper and psts

--- src/main/java/com/example/dao/NutritionDaoImpl.java
+++ src/main/java/com/example/dao/NutritionDaoImpl.java
@@ -1,6 +1,8 @@
 package com.example.dao;
 
 import com.example.domain.Nutrition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -8,58 +10,36 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by MattyG on 5/8/17.
  */
 @Repository
 public class NutritionDaoImpl implements NutritionDao {
+    private static final Logger log = LoggerFactory.getLogger(NutritionDaoImpl.class);
 
     @Autowired
     JdbcTemplate jdbcTemplate;
-
     @Override
     public void add(Nutrition nutrition) {
-        String sql = "INSERT INTO nutrition (product, calories, carbs) VALUES ('";
-        sql = sql + nutrition.getProduct();
-        sql = sql + "' ,";
-        sql = sql + nutrition.getCalories();
-        sql = sql + ", ";
-        sql = sql + nutrition.getCarbs();
-        sql = sql + ")";
-        System.out.println("\n!!!\n the insert sql is: " + sql);
-        jdbcTemplate.update(sql);
+        log.info("adding " + nutrition);
+        jdbcTemplate.update("INSERT INTO nutrition(product, calories, carbs) VALUES (?,?,?)",
+                nutrition.getProduct(),
+                nutrition.getCalories(),
+                nutrition.getCarbs());
     }
+
     @Override
     public List<Nutrition> findAll() {
-
-        List<Map<String,Object>> results = jdbcTemplate.queryForList("select * from nutrition");
-        List<Nutrition> nutritions = new ArrayList<>();
-
-        for(Map<String, Object> map : results){
-            Nutrition nut = new Nutrition();
-            nut.setId((int)map.get("id"));
-            nut.setCalories((int)map.get("calories"));
-            nut.setCarbs((int)map.get("carbs"));
-            nut.setProduct((String)map.get("product"));
-            nutritions.add(nut);
-        }
-        System.out.println(results);
+        List<Nutrition> nutritions = this.jdbcTemplate.query(
+                "select * from nutrition",
+                new NutritionMapper());
+        log.info("Found " + nutritions.size() + " nutritions");
         return nutritions;
     }
 
-    /*
-
-    public class Nutrition {
-    private long id;
-    private String product;
-    private int calories;
-    private int carbs;
-     */
-    private static class PersonMapper implements RowMapper<Nutrition> {
+    private static class NutritionMapper implements RowMapper<Nutrition> {
         @Override
         public Nutrition mapRow(ResultSet rs, int rowNum) throws SQLException {
             Nutrition nutrition = new Nutrition();
@@ -71,41 +51,4 @@ public class NutritionDaoImpl implements NutritionDao {
         }
     }
 
-
-    /*
-    @Override
-    public void add(Person person) {
-        log.info("adding " + person);
-        jdbcTemplate.update("INSERT INTO person(name, dob, gender) VALUES (?,?,?)",
-                person.getName(),
-                person.getDob(),
-                person.getGender());
-    }
-    @Override
-    public List<Person> find() {
-        List<Person> persons = this.jdbcTemplate.query(
-                "select * from person",
-                new PersonMapper());
-        log.info("Found " + persons.size() + " persons");
-        return persons;
-    }
-
-    @Override
-    public void add(Nutrition person) {
-        log.info("adding " + person);
-        jdbcTemplate.update("INSERT INTO person(name, dob, gender) VALUES (?,?,?)",
-                person.getName(),
-                person.getDob(),
-                person.getGender());
-    }
-    @Override
-    public List<Nutrition> find() {
-        List<Nutrition> persons = this.jdbcTemplate.query(
-                "select * from person",
-                new PersonMapper());
-        log.info("Found " + persons.size() + " persons");
-        return persons;
-    }
-
-     */
 }
